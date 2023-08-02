13b1fb042105c7ba28c3921147cdcb8040da5131
Manuel Carrer <manuelca@kjemi.uio.no>
2023-08-02 14:39:53 +0200

    Add day 14.2

--- day_14/rust/src/main.rs
+++ day_14/rust/src/main.rs
@@ -2,7 +2,8 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn draw_scan(coords: Vec<&str>, scan: &mut [[char; 170]; 150]) {
+fn draw_scan(coords: Vec<&str>, scan: &mut [[char; 170]; 1000]) -> usize {
+    let mut max_y: usize = 0;
     for i in 1..coords.len() {
         let p1: Vec<_> = coords[i]
             .split(',')
@@ -12,53 +13,66 @@ fn draw_scan(coords: Vec<&str>, scan: &mut [[char; 170]; 150]) {
             .split(',')
             .map(|x| x.parse::<usize>().unwrap())
             .collect();
+        if p1[1] > max_y {
+            max_y = p1[1] as usize;
+        }
+        if p2[1] > max_y {
+            max_y = p1[1] as usize;
+        }
         if p1[0] == p2[0] {
             if p1[1] > p2[1] {
                 for j in p2[1]..=p1[1] {
-                    scan[p1[0] - 450][j] = '#';
+                    scan[p1[0]][j] = '#';
                 }
             } else {
                 for j in p1[1]..=p2[1] {
-                    scan[p1[0] - 450][j] = '#';
+                    scan[p1[0]][j] = '#';
                 }
             }
         } else {
             if p1[0] > p2[0] {
                 for j in p2[0]..=p1[0] {
-                    scan[j - 450][p1[1]] = '#';
+                    scan[j][p1[1]] = '#';
                 }
             } else {
                 for j in p1[0]..=p2[0] {
-                    scan[j - 450][p1[1]] = '#';
+                    scan[j][p1[1]] = '#';
                 }
             }
         }
     }
+    max_y
 }
 
-fn simulate_sand(scan: &mut [[char; 170]; 150]) -> u32 {
+fn simulate_sand(scan: &mut [[char; 170]; 1000]) -> u32 {
     let mut n_grains: u32 = 0;
     loop {
-        let mut grain: [usize; 2] = [50, 0];
+        let mut grain: [usize; 2] = [500, 0];
         loop {
+            grain[1] += 1;
+
+            // Check abyss
+            if grain[1] == scan[0].len() - 1 {
+                return n_grains;
+            }
+
             // Check down
-            if ['#', 'o'].contains(&scan[grain[0]][grain[1]]) {
+            if ['#', '.'].contains(&scan[grain[0]][grain[1]]) {
                 // Check down-left
-                if ['#', 'o'].contains(&scan[grain[0] - 1][grain[1]]) {
+                if ['#', '.'].contains(&scan[grain[0] - 1][grain[1]]) {
                     // Check down-right
-                    if ['#', 'o'].contains(&scan[grain[0] + 1][grain[1]]) {
-                        scan[grain[0]][grain[1] - 1] = 'o';
+                    if ['#', '.'].contains(&scan[grain[0] + 1][grain[1]]) {
+                        // Check if starting point
+                        if scan[grain[0]][grain[1] - 1] == 'S' {
+                            return n_grains + 1;
+                        }
+                        scan[grain[0]][grain[1] - 1] = '.';
                         break;
                     } else {
                         grain[0] += 1;
                     }
                 } else {
                     grain[0] -= 1;
-                }
-            } else {
-                grain[1] += 1;
-                if grain[1] == scan[0].len() - 1 {
-                    return n_grains;
                 }
             }
         }
@@ -71,41 +85,31 @@ fn main() {
     let fname = args.nth(1).expect("Provide input file as first argument");
     let file = File::open(fname).expect("File should exist.");
     let reader = BufReader::new(file);
-    let mut scan = [['.'; 170]; 150];
+    let mut scan = [[' '; 170]; 1000];
 
-    // let test_input = vec![
-    //     "498,4 -> 498,6 -> 496,6",
-    //     "503,4 -> 502,4 -> 502,9 -> 494,9",
-    // ];
-
+    let mut max_y: Vec<usize> = vec![];
     for line_ in reader.lines() {
         let line = line_.unwrap();
         let split: Vec<_> = line.split(" -> ").collect();
-        draw_scan(split, &mut scan);
+        max_y.push(draw_scan(split, &mut scan));
     }
-    // for line in &test_input {
-    //     let split: Vec<_> = line.split(" -> ").collect();
-    //     draw_scan(split, &mut scan);
-    // }
 
-    scan[50][0] = 'S';
+    // Part 2, comment out the next 4 lines for part 1
+    let max_y = 2 + *max_y.iter().max().unwrap();
+    for row in &mut scan {
+        row[max_y] = '#'
+    }
 
+    scan[500][0] = 'S';
     let n_grains = simulate_sand(&mut scan);
+
     // Print transposed array
     for col in 0..scan[0].len() {
-        for row in &scan {
+        for row in &scan[450..600] {
             print!("{}", row[col]);
         }
         println!();
     }
 
-    // for row in &scan {
-    //     for col in row {
-    //         print!("{}", col);
-    //     }
-    //     println!();
-    // }
-
     println!("{}", n_grains);
-    // 727 too low
 }
