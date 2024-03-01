60551be29afca12fd681a2af5c5e9aba558970ff
Manuel Carrer <manuel.carrer@kjemi.uio.no>
2024-03-01 14:54:49 +0100

    Update old cpp solution

--- day_7/cpp/main.cpp
+++ day_7/cpp/main.cpp
@@ -1,109 +1,119 @@
-#include <algorithm>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
-struct directory {
-  std::string name;
-  int parent;
-  std::vector<int> subdirs;
-  int depth;
-  long size;
+class FileSystem {
+public:
+  FileSystem() : dir(nullptr){};
 
-  directory() {
-    name = "";
-    parent = -1;
-    subdirs = {};
-    depth = 0;
-    size = 0;
+  void cd(std::string name) {
+    if (dir == nullptr) {
+      dir = new Directory(name, dir);
+    } else if (name == "..") {
+      dir = dir->parent;
+    } else {
+      Directory *new_dir = new Directory(name, dir);
+      dir->subdirs.push_back(new_dir);
+      dir = new_dir;
+    }
   }
+
+  void to_root() {
+    while (dir->parent != nullptr) {
+      dir = dir->parent;
+    }
+    // Calculate the correct sizes
+    dir->add_subdirs_size();
+  }
+
+  void update_size(std::string s) { dir->size += std::stol(s); }
+  uint32_t get_size() { return dir->size; }
+  uint32_t sum_dirs_under(uint32_t s) { return dir->sum_dirs_under(s); }
+  uint32_t smallest_dir_above(uint32_t s) { return dir->smallest_dir_above(s); }
+
+private:
+  struct Directory {
+    Directory *parent;
+    std::vector<Directory *> subdirs;
+    std::string name;
+    uint32_t size;
+
+    Directory(std::string n, Directory *p)
+        : parent(p), subdirs({}), name(n), size(0){};
+
+    void add_subdirs_size() {
+      for (auto d : subdirs) {
+        d->add_subdirs_size();
+        size += d->size;
+      }
+    }
+    uint32_t sum_dirs_under(uint32_t s) {
+      uint32_t result = 0;
+
+      for (auto d : subdirs) {
+        result += d->sum_dirs_under(s);
+      }
+
+      if (size < s) {
+        result += size;
+      }
+      return result;
+    }
+
+    uint32_t smallest_dir_above(uint32_t s) {
+      uint32_t result = size;
+      uint32_t temp;
+
+      for (auto d : subdirs) {
+        temp = d->smallest_dir_above(s);
+        if (temp >= s && temp < result) {
+          result = temp;
+        }
+      }
+      return result;
+    }
+  };
+
+  Directory *dir; // current directory
 };
 
 int main() {
-  long result = 0;
+  size_t idx;
   std::ifstream infile("../input.txt");
   std::string line;
-  size_t idx;
   std::vector<std::string> split;
-
-  directory dir;
-  std::vector<directory> filesystem;
-  int depth = -1, max_depth = 0;
-  int parent_dir = -1, index = -1;
+  FileSystem filesystem;
 
   while (std::getline(infile, line)) {
-    // Split line
-    split.clear();
+    // Parse line
     while ((idx = line.find(' ')) != std::string::npos) {
       split.push_back(line.substr(0, idx));
       line.erase(0, idx + 1);
     }
     split.push_back(line);
 
+    // Build filesystem tree
     if (split[0] == "$") {
       if (split[1] == "cd") {
-        if (split[2] == "..") {
-          // Reset parent index when going up
-          --depth;
-          parent_dir = filesystem[parent_dir].parent;
-        } else {
-          // Update directory
-          ++depth;
-          ++index;
-          dir.name = split[2];
-          dir.parent = parent_dir;
-          dir.depth = depth;
-          dir.size = 0;
-          dir.subdirs = {};
-          filesystem.push_back(dir);
-          // Link subdir to parent
-          if (dir.parent >= 0) {
-            filesystem[parent_dir].subdirs.push_back(index);
-          }
-          // Update directory depth
-          if (depth > max_depth) {
-            max_depth = depth;
-          }
-        }
-      } else if (split[1] == "ls") {
-        parent_dir = index;
+        filesystem.cd(split[2]);
       }
-    } else if (split[0] == "dir") {
-      continue;
-    } else {
-      filesystem[index].size += std::stol(split[0]);
+    } else if (split[0] != "dir") {
+      filesystem.update_size(split[0]);
     }
+    split.clear();
   }
   infile.close();
+  filesystem.to_root();
 
-  // Update dir sizes starting from deeper subdirs
-  while (max_depth >= 0) {
-    for (size_t i = 0; i < filesystem.size(); ++i) {
-      if (filesystem[i].depth == max_depth) {
-        for (size_t j = 0; j < filesystem[i].subdirs.size(); ++j) {
-          filesystem[i].size += filesystem[filesystem[i].subdirs[j]].size;
-        }
-        // Part 1: total sum of dir with sizes less that 100K
-        // if (filesystem[i].size <= 100000) {
-        //   result += filesystem[i].size;
-        // }
-      }
-    }
-    --max_depth;
-  }
+  // Part 1: total sum of dir with sizes less that 100K
+  uint32_t result = filesystem.sum_dirs_under(100'000);
+  std::cout << "Part 1: " << result << "\n";
 
   // Part 2: find smallest directory size that frees at least 'req_size'
-  long req_size = filesystem[0].size - 40000000;
-  result = filesystem[0].size;
-  for (size_t i = 1; i < filesystem.size(); ++i) {
-    if (filesystem[i].size >= req_size) {
-      if (result > filesystem[i].size) {
-        result = filesystem[i].size;
-      }
-    }
-  }
-  std::cout << result << "\n";
+  uint32_t req_size = filesystem.get_size() - 40'000'000;
+  result = filesystem.smallest_dir_above(req_size);
+  std::cout << "Part 2: " << result << "\n";
   return 0;
 }
